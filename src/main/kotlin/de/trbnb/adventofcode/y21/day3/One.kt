96004970b9e3e3c6d9b4f8f321c13d66d7bdbdfe
Thorben Buchta <thorbenbuchta@gmail.com>
2021-12-03 16:33:53 +0100

    add solution for day three

---
+++ src/main/kotlin/de/trbnb/adventofcode/y21/day3/One.kt
@@ -0,0 +1,22 @@
+package de.trbnb.adventofcode.y21.day3
+
+import de.trbnb.adventofcode.y21.resourceFile
+
+fun main() {
+    resourceFile("2021_three_one.txt").readLines()
+        .map(String::toList)
+        .rotate { it == '1' }
+        .map { column -> mapOf(false to column.count { !it }, true to column.count { it }) }
+        .map { column -> column.maxByOrNull { it.value }?.key ?: false }
+        .let { list -> listOf(list.joinToString(), list.map { !it }.joinToString()) }
+        .map { it.toInt(radix = 2) }
+        .let { (most, least) -> most * least }
+        .let { println(it) }
+}
+
+fun Boolean.toInt() = if (this) 1 else 0
+fun List<Boolean>.joinToString() = joinToString(separator = "") { it.toInt().toString() }
+
+fun <T, R> List<List<T>>.rotate(transform: (T) -> R): List<List<R>> {
+    return List(first().size) { index -> map { transform(it[index]) } }
+}
---
+++ src/main/kotlin/de/trbnb/adventofcode/y21/day3/Two.kt
@@ -0,0 +1,40 @@
+package de.trbnb.adventofcode.y21.day3
+
+import de.trbnb.adventofcode.y21.resourceFile
+
+fun main() {
+    resourceFile("2021_three_one.txt").readLines()
+        .map { line -> line.map { it == '1' } }
+        .let { lines -> findExtreme(lines, true) to findExtreme(lines, false) }
+        .let { (first, second) -> first * second }
+        .let(::println)
+}
+
+fun findExtreme(allLines: List<List<Boolean>>, max: Boolean): Int {
+    var allowedIndices = allLines.indices.toList()
+    var column = 0
+
+    while(allowedIndices.size > 1) {
+        val rowNumbersAndValues = allLines.mapIndexed { index, list -> index to list[column] }
+            .filterIndexed { index, _ -> index in allowedIndices }
+
+        val extreme = rowNumbersAndValues.groupBy { it.second }
+            .map { (key, value) -> key to value.size }
+            .let { pairs ->
+                val zeros = pairs.firstOrNull { !it.first }?.second ?: 0
+                val ones = pairs.firstOrNull { it.first }?.second ?: 0
+                if (max) {
+                    zeros <= ones
+                } else {
+                    zeros > ones
+                }
+            }
+
+        allowedIndices = rowNumbersAndValues.filter { (_, value) -> value != extreme }
+            .map { (row, _) -> row }
+
+        column++
+    }
+
+    return allLines[allowedIndices.first()].joinToString().toInt(radix = 2)
+}
